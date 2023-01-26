a2cfa73ddaca8e19801f1b98a809cf74fa79d21a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-01-26 19:52:46 +0700

    add splashcreen dependency

---
+++ lib/view/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    title: "FASHION STORE",
+    home: null,
+    routes: {
+      "/login": (ctx) => Text("data"),
+    },
+  ));
+}
